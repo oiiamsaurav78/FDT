eb9ecf679b2cfb62412bffd643db5053e5da614b
oiiamsaurav78 <122017379+oiiamsaurav78@users.noreply.github.com>
2023-09-10 17:53:44 +0530

    Delete fdt directory

--- fdt/index.js
+++
@@ -1,9 +0,0 @@
-let menu =document.querySelector("#menu-icon");
-let navbar =document.querySelector(".navbar");
-menu.addEventListener("click",function(){
-    navbar.classList.toggle("active");
-    console.log("click")
-});
-window.onscroll=()=>{
-    navbar.classList.remove("active");
-}
